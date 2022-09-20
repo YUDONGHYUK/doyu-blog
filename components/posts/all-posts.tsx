e04bb39f25a21ddd27cf086b163a765b76033419
doyu <ydh0905@nate.com>
2022-09-20 13:25:33 +0900

    feat: create PostPage(AllPosts) component

---
+++ components/posts/all-posts.tsx
@@ -0,0 +1,30 @@
+import PostGrid from './posts-grid';
+import { Post } from '../../type';
+import styled from 'styled-components';
+
+type AllPostsType = {
+  posts: Post[];
+};
+
+const AllPosts = ({ posts }: AllPostsType) => {
+  return (
+    <Container>
+      <Title>All Posts</Title>
+      <PostGrid posts={posts} />
+    </Container>
+  );
+};
+
+export default AllPosts;
+
+const Container = styled.section`
+  max-width: 48rem;
+  margin: auto;
+  padding: 0 1.5rem;
+`;
+
+const Title = styled.h2`
+  padding: 2rem 0;
+  font-size: ${({ theme }) => theme.font.size12};
+  text-align: center;
+`;
