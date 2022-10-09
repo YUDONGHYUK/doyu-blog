070b427a64021c8ae885854d1e050938d0f5857e
doyu <ydh0905@nate.com>
2022-10-09 20:22:41 +0900

    Modify regex in generateSlug

--- lib/generateSlug.ts
+++ lib/generateSlug.ts
@@ -1,5 +1,10 @@
 export const generateSlug = (str: string) => {
-  const slug = str?.replace(/ /g, '-')?.toLowerCase();
+  let slug = str?.replace(/ /g, '-').toLowerCase();
+
+  slug = slug
+    ?.replace(/[^ㄱ-ㅎㅏ-ㅣ가-힣a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-');
 
   return slug;
 };
