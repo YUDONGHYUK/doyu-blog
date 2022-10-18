6f0c7d042eda62dde11e3e0cf886c3189063bbaa
doyu <ydh0905@nate.com>
2022-10-18 21:08:48 +0900

    Create post search

--- components/posts/AllPosts.tsx
+++ components/posts/AllPosts.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import PostsGrid from './PostsGrid';
 import { Post } from '../../types';
+import SearchButton from '../posts-page/SearchButton';
+import CloseIcon from '../icons/close';
 import styled from 'styled-components';
 
 type AllPostsProps = {
@@ -7,10 +10,39 @@ type AllPostsProps = {
 };
 
 const AllPosts = ({ posts }: AllPostsProps) => {
+  const [search, setSearch] = useState('');
+
+  const searchResults = (search: string, posts: Post[]) => {
+    return posts.filter((post) =>
+      post?.frontMatter?.title.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredPosts = searchResults(search, posts);
+
   return (
     <Container>
       <Title>All Posts</Title>
-      <PostsGrid posts={posts} />
+      <InputWrapper>
+        <Input
+          type="text"
+          placeholder="Search Posts"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <SearchButton search={search} setSearch={setSearch} />
+      </InputWrapper>
+      {filteredPosts.length > 0 ? (
+        <PostsGrid posts={filteredPosts} />
+      ) : (
+        <NoPosts>
+          No posts found.
+          <button onClick={() => setSearch('')}>
+            <CloseIcon size={16} />
+            Clear Search
+          </button>
+        </NoPosts>
+      )}
     </Container>
   );
 };
@@ -20,7 +52,65 @@ export default AllPosts;
 const Container = styled.section``;
 
 const Title = styled.h2`
+  margin-bottom: 0;
   padding: 1rem 0;
   font-size: ${({ theme }) => theme.font10};
-  text-align: center;
+  text-align: start;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  margin-bottom: 2rem;
+
+  .search,
+  .close {
+    position: absolute;
+    top: 13.75px;
+    right: 0;
+    display: flex;
+    align-items: center;
+    width: 35px;
+    height: 25px;
+  }
+
+  svg {
+    fill: ${({ theme }) => theme.text2};
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  margin: 0.5rem 0;
+  padding: 0.5rem;
+  border: 1px solid ${({ theme }) => theme.bg_element};
+  border-radius: 0.25rem;
+  background-color: ${({ theme }) => theme.bg_element};
+  font-size: ${({ theme }) => theme.font4};
+  color: ${({ theme }) => theme.text2};
+
+  :focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const NoPosts = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: ${({ theme }) => theme.font4};
+  line-height: 1rem;
+
+  button {
+    display: flex;
+    align-items: center;
+    margin-left: 0.5rem;
+    color: ${({ theme }) => theme.primary};
+    font-size: ${({ theme }) => theme.font4};
+    line-height: 1rem;
+
+    svg {
+      margin-right: 0.2rem;
+      fill: ${({ theme }) => theme.primary};
+    }
+  }
 `;
